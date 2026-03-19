fa6f3b55988b1749a0a502ad01289238334488cb
Maksud Chowdhury <maksudul.haquechowdhury@gmail.com>
2026-03-19 12:47:54 +0600

    revised script

--- linux_docker_deployment_Vectra.sh
+++ linux_docker_deployment_Vectra.sh
@@ -9,13 +9,13 @@ echo "==> Creating persistent directories..."
 sudo mkdir -p /app/vectra/sqlite_data /app/vectra/chroma_data
 
 echo "==> Setting permissions..."
-sudo chmod -R 777 /app/vectra/sqlite_data /app/vectra/chroma_data
-
-echo "==> Stopping existing containers..."
-docker compose down --remove-orphans || true
+sudo chmod -R 775 /app/vectra/sqlite_data /app/vectra/chroma_data
 
 echo "==> Cleaning old data..."
 sudo rm -rf /app/vectra/sqlite_data/* /app/vectra/chroma_data/*
+
+echo "==> Stopping existing containers..."
+docker compose down --remove-orphans || true
 
 echo "==> Building and starting containers..."
 docker compose up --build -d
